512e5210f4132d77ef9f76bbd6353b1ac094d687
Yazs-17 <15957544659@163.com>
2025-10-21 10:29:21 +0800

    feat: integrate content and output a comprehensive JS library

---
+++ Gooact/gooact.js
@@ -0,0 +1,209 @@
+/*************************************************************
+ * Gooact.js - A Minimal React-like Virtual DOM Library
+ * -----------------------------------------------------------
+ * Features:
+ *  1. JSX-like Virtual DOM via createElement
+ *  2. render() - Mount virtual DOM to real DOM
+ *  3. patch()  - Efficient DOM diff & update
+ *  4. Component - Stateful components with lifecycle hooks
+ *************************************************************/
+
+export const createElement = (type, props, ...children) => ({
+	type,
+	props: props || {},
+	children: children.flat()
+});
+
+/***************** render *****************/
+export const render = (vdom, parent = null) => {
+	if (typeof vdom === 'string' || typeof vdom === 'number') {
+		const textNode = document.createTextNode(vdom);
+		if (parent) parent.appendChild(textNode);
+		return textNode;
+	}
+
+	if (typeof vdom.type === 'function') {
+		return Component.render(vdom, parent);
+	}
+
+	const dom = document.createElement(vdom.type);
+
+	for (const prop in vdom.props) {
+		setAttribute(dom, prop, vdom.props[prop]);
+	}
+
+	vdom.children.map(child => render(child, dom));
+	if (parent) parent.appendChild(dom);
+	return dom;
+};
+
+/***************** setAttribute *****************/
+export const setAttribute = (dom, name, value) => {
+	if (name === 'style' && typeof value === 'object') {
+		Object.assign(dom.style, value);
+	} else if (name.startsWith('on') && typeof value === 'function') {
+		const event = name.slice(2).toLowerCase();
+		dom.addEventListener(event, value);
+	} else if (name !== 'key') {
+		dom.setAttribute(name, value);
+	}
+};
+
+/***************** patch *****************/
+export const patch = (dom, vdom, parent = dom.parentNode) => {
+	const replace = parent
+		? el => (parent.replaceChild(el, dom) && el)
+		: el => el;
+
+	if (typeof vdom === 'object' && typeof vdom.type === 'function') {
+		return Component.patch(dom, vdom, parent);
+	} else if (typeof vdom !== 'object' && dom instanceof Text) {
+		return dom.textContent !== vdom ? replace(render(vdom, parent)) : dom;
+	} else if (typeof vdom === 'object' && dom instanceof Text) {
+		return replace(render(vdom, parent));
+	} else if (typeof vdom === 'object' && dom.nodeName !== vdom.type.toUpperCase()) {
+		return replace(render(vdom, parent));
+	} else if (typeof vdom === 'object' && dom.nodeName === vdom.type.toUpperCase()) {
+		const pool = {};
+		const active = document.activeElement;
+
+		Array.from(dom.childNodes).forEach((child, index) => {
+			const key = child.__gooactKey || `__index_${index}`;
+			pool[key] = child;
+		});
+
+		vdom.children.forEach((child, index) => {
+			const key = (child.props && child.props.key) || `__index_${index}`;
+			dom.appendChild(pool[key] ? patch(pool[key], child) : render(child, dom));
+			delete pool[key];
+		});
+
+		for (const key in pool) {
+			const instance = pool[key].__gooactInstance;
+			if (instance) instance.componentWillUnmount();
+			pool[key].remove();
+		}
+
+		for (const attr of Array.from(dom.attributes)) dom.removeAttribute(attr.name);
+		for (const prop in vdom.props) setAttribute(dom, prop, vdom.props[prop]);
+
+		active.focus();
+		return dom;
+	}
+};
+
+/***************** Component *****************/
+export class Component {
+	constructor(props) {
+		this.props = props || {};
+		this.state = null;
+	}
+
+	static render (vdom, parent = null) {
+		const props = Object.assign({}, vdom.props, { children: vdom.children });
+
+		// class component
+		if (Component.isPrototypeOf(vdom.type)) {
+			const instance = new vdom.type(props);
+			instance.componentWillMount();
+			instance.base = render(instance.render(), parent);
+			instance.base.__gooactInstance = instance;
+			instance.base.__gooactKey = vdom.props.key;
+			instance.componentDidMount();
+			return instance.base;
+		}
+		// function component
+		else {
+			return render(vdom.type(props), parent);
+		}
+	}
+
+	static patch (dom, vdom, parent = dom.parentNode) {
+		const props = Object.assign({}, vdom.props, { children: vdom.children });
+
+		if (dom.__gooactInstance && dom.__gooactInstance.constructor === vdom.type) {
+			dom.__gooactInstance.componentWillReceiveProps(props);
+			dom.__gooactInstance.props = props;
+			return patch(dom, dom.__gooactInstance.render(), parent);
+		} else if (Component.isPrototypeOf(vdom.type)) {
+			const ndom = Component.render(vdom, parent);
+			return parent ? (parent.replaceChild(ndom, dom) && ndom) : ndom;
+		} else if (!Component.isPrototypeOf(vdom.type)) {
+			return patch(dom, vdom.type(props), parent);
+		}
+	}
+
+	setState (nextState) {
+		if (this.base && this.shouldComponentUpdate(this.props, nextState)) {
+			const prevState = this.state;
+			this.componentWillUpdate(this.props, nextState);
+			this.state = nextState;
+			patch(this.base, this.render());
+			this.componentDidUpdate(this.props, prevState);
+		} else {
+			this.state = nextState;
+		}
+	}
+
+	shouldComponentUpdate (nextProps, nextState) {
+		return nextProps !== this.props || nextState !== this.state;
+	}
+
+	componentWillReceiveProps (nextProps) { }
+	componentWillUpdate (nextProps, nextState) { }
+	componentDidUpdate (prevProps, prevState) { }
+	componentWillMount () { }
+	componentDidMount () { }
+	componentWillUnmount () { }
+}
+
+/***************** Gooact namespace *****************/
+const Gooact = {
+	createElement,
+	render,
+	patch,
+	Component,
+	setAttribute
+};
+
+export default Gooact;
+
+
+/*
+
+the way of importing 1: 
+
+<script type="module">
+import Gooact, { createElement, Component, render } from './gooact.js';
+
+class Counter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { count: 0 };
+	}
+
+	increment = () => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
+	render() {
+		return createElement(
+			"div",
+			{ style: { textAlign: "center", marginTop: "40px" } },
+			createElement("h1", null, "Count: ", this.state.count),
+			createElement("button", { onClick: this.increment }, "Increment")
+		);
+	}
+}
+
+const root = document.getElementById("root");
+render(createElement(Counter, null), root);
+</script>
+
+
+
+the way of importing 2: 
+IIFE
+
+
+*/
